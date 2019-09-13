2273fc39e7fba696c2c6c8eb9cbae2a3dc3dcfc6
Shailesh Kumar <shaileshk.2140@gmail.com>
2019-09-14 03:57:36 +0530

    Updates

---
+++ precache-manifest.4945578344ce4526d6b0efb3e2aa3315.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03690d2e5addf306bc69720aa33af49b",
+    "url": "/shaileshk-2140/home/index.html"
+  },
+  {
+    "revision": "095db6c3d6b38191325d",
+    "url": "/shaileshk-2140/home/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "50092569e8b63addb95c",
+    "url": "/shaileshk-2140/home/static/js/2.7c314d9f.chunk.js"
+  },
+  {
+    "revision": "095db6c3d6b38191325d",
+    "url": "/shaileshk-2140/home/static/js/main.d713d31e.chunk.js"
+  },
+  {
+    "revision": "dfcb94bfeb77802052f5",
+    "url": "/shaileshk-2140/home/static/js/runtime~main.8a46675f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shaileshk-2140/home/static/media/logo.5d5d9eef.svg"
+  }
+]);
