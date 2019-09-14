2786097c4feb8609936c31c2eea00bd6c0cf37ac
Shailesh Kumar <shaileshk.2140@gmail.com>
2019-09-14 05:49:14 +0530

    Updates

---
+++ precache-manifest.f640118ba37ba02e24df23e2d39ef5e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9177e234282fd34bd00a526eb915803e",
+    "url": "/shaileshk-2140/home.git/index.html"
+  },
+  {
+    "revision": "4956ab591fcc218e6c8e",
+    "url": "/shaileshk-2140/home.git/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "50092569e8b63addb95c",
+    "url": "/shaileshk-2140/home.git/static/js/2.7c314d9f.chunk.js"
+  },
+  {
+    "revision": "4956ab591fcc218e6c8e",
+    "url": "/shaileshk-2140/home.git/static/js/main.9955bd3c.chunk.js"
+  },
+  {
+    "revision": "3d32806ff6bdb08c5167",
+    "url": "/shaileshk-2140/home.git/static/js/runtime~main.453612b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shaileshk-2140/home.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.58e73c246cd2981da44ed60ee6e28b20.js"
+  "/shaileshk-2140/home.git/precache-manifest.f640118ba37ba02e24df23e2d39ef5e1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shaileshk-2140/home.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
